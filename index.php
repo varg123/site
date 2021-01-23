3d55af938c825e5fdec2f1ce85c782b758589f1d
Shkilev Andrey <shkilev.studiofact@gmail.com>
2021-01-23 22:40:20 +0500

    авторизация для теста

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
-$USER->Authorize(1);
+
+
 ?>
 
 <?if (IsModuleInstalled("advertising")):?>
---
+++ test/index.php
@@ -0,0 +1,4 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$USER->Authorize(1);
+
